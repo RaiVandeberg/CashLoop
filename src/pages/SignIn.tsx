1173e3ec736726345e58d73a81695c360452852b
Raí <devvandenberg>
2025-03-28 20:54:47 -0300

    aplicando useActionState

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,36 +1,46 @@
-import { useState } from "react"
+import { useActionState } from "react"
 
 import { Input } from "../components/Input"
 
 import { Button } from "../components/Button"
 
 export function SignIn() {
+    const [ state, formAction, isLoading ] = useActionState(signIn, {email: "", password:""})
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
-
-    function onSubmit(e: React.FormEvent) {
-        e.preventDefault()
-        console.log({email, password})
+   
+    
+    async function signIn(prevState: any, formData: FormData) {
+       const email = formData.get("email")
+         const password = formData.get("password")
+        
+         return { email, password }
+        
+       
+        
 
     }
 
-    return <form onSubmit={onSubmit}
+    return <form action={formAction}
         className="w-full flex flex-col gap-4">
         <Input required 
+        name="email"
         legend="E-mail" 
         type="email"   
         placeholder="seu@gmail.com" 
-        onChange={e => setEmail(e.target.value)}
+        defaultValue={String(state?.email)}
+       
+        
         />
 
         <Input required 
+        name="password"
         legend="Senha" 
         type="password"   
         placeholder="Digite sua senha" 
         min={6}
-        onChange={e => setPassword(e.target.value)}
+        defaultValue={String(state?.password)}
+        
+       
         />
 
         <Button type="submit" isLoading={isLoading}>Entrar</Button>
